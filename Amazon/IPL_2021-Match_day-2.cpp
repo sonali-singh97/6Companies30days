55721a333eb1240ee476bdf97371013f41485916
Sonali Singh <sonali122000@gmail.com>
2022-01-06 22:58:27 +0530

    Add IPL 2021 Match day 2 question

---
+++ Amazon/IPL_2021-Match_day-2.cpp
@@ -0,0 +1,39 @@
+/*
+Question: IPL 2021 - Match Day 2 
+Link: https://practice.geeksforgeeks.org/problems/deee0e8cf9910e7219f663c18d6d640ea0b87f87/1/#
+*/
+
+
+class Solution {
+  public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+      
+        deque<int> dq;
+        vector<int> output;
+        int i = 0;
+        
+   
+        for (; i < n; i ++){
+            
+            // delete out of the range indexes
+            while(!dq.empty() && dq.front() <= i-k) dq.pop_front();
+            
+            // delete indexes at which value is less than current value
+            while(!dq.empty() && arr[dq.back()] <= arr[i]) dq.pop_back();
+            
+            dq.push_back(i);
+            
+            if(i >= k-1){
+                output.push_back(arr[dq.front()]);
+            }
+            
+        }
+        
+        return output;
+    }
+};
+
+/*
+Time Complexity = O(n) => Every element is added or removed atmost once (2n)
+Space : O(k)
+*/
